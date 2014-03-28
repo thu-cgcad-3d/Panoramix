5342e0acc25440f4df4cfd51a7fe1a3ada654554
Y.H <yangh2007@gmail.com>
2014-03-29 00:21:01 +0800

    fix for mac

--- src/vis/visualize3d.cpp
+++ src/vis/visualize3d.cpp
@@ -169,7 +169,7 @@ namespace panoramix {
                         glEnable(GL_CULL_FACE);
                         glEnable(GL_DEPTH_TEST);
                         glEnable(GL_STENCIL_TEST);
-                        glEnable(GL_ALPHA_TEST_QCOM);
+                        //glEnable(GL_ALPHA_TEST_QCOM);
 
                         glEnable(GL_BLEND);
                         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
